d53368036b4d902dbbfeb85782ba8d15190fff0d
chetan9096 <badgujarchetan80@gmail.com>
2024-10-25 16:23:33 +0530

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,79 @@
+terraform {
+  backend "s3" {
+    bucket         = "467.devops.candidate.exam"
+    region         = "ap-south-1"
+    key            = "chetan.badgujar"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = data.aws_vpc.vpc.id
+  cidr_block              = "10.0.0.0/16"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Private Subnet"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = data.aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = data.aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "Private Route Table"
+  }
+}
+
+resource "aws_route_table_association" "private_route_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_security_group" "lambda_sg" {
+  name        = "lambda_sg"
+  description = "Security Group for Lambda inside VPC"
+  vpc_id      = data.aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Lambda Security Group"
+  }
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "MyLambdaFunction"
+  role          = data.aws_iam_role.lambda.arn
+  handler       = "index.handler"
+  runtime       = "python3.9"
+
+  # Path to your Lambda function deployment package
+  filename      = "lambda_function.zip"
+  
+  vpc_config {
+    subnet_ids         = [aws_subnet.private_subnet.id]
+    security_group_ids = [aws_security_group.lambda_sg.id]
+  }
+
+  tags = {
+    Name = "My Lambda Function"
+  }
+}
