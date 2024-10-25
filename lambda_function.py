e4b47ae9fe083d1bd6ba4635bdeb6f80e0615df6
chetan9096 <badgujarchetan80@gmail.com>
2024-10-25 18:31:49 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+import base64
 
 def lambda_handler(event, context):
     try:
@@ -8,26 +9,30 @@ def lambda_handler(event, context):
             raise ValueError("Environment variable 'SUBNET_ID' is not set.")
         
         subnet_id = os.environ['SUBNET_ID']
-        
-        print(f"Subnet ID: {subnet_id}")  
+        print(f"Subnet ID: {subnet_id}") 
         
         payload = {
             "subnet_id": subnet_id,
-            "name": "<Your Full Name>",
-            "email": "<Your Email Address>"
+            "name": "Chetan Badgujar",
+            "email": "badgujarchetan80@gmail.com"
         }
+        print(f"Payload: {json.dumps(payload)}")  
         
-        print(f"Payload: {json.dumps(payload)}")
-
         response = requests.post(
             "https://bc1yy8dzsg.execute-api.eu-west-1.amazonaws.com/v1/data",
             headers={'X-Siemens-Auth': 'test'},
             json=payload
         )
         
-        print(f"Response: {response.text}")  
-        
         response.raise_for_status()  
+        
+        print(f"API Response: {response.json()}")
+        
+        log_result = response.headers.get("X-Amzn-Trace-Id")  
+
+        # Decode the log_result 
+        decoded_log_result = base64.b64decode(log_result).decode('utf-8')
+        print(f"Decoded Log Result: {decoded_log_result}")
 
         return {
             "statusCode": 200,
